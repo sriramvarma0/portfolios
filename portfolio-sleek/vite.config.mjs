2ac6513dacc35bbb9991ad872a14463dfa7e5aab
sriramvarma0 <bhupathirajusriramvarma@gmail.com>
2025-12-09 13:42:42 +0530

    DEPLOYERRORHANDLE

--- portfolio-sleek/vite.config.mjs
+++ portfolio-sleek/vite.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/portfolios/portfolio-sleek/',   // ✅ REQUIRED for your folder structure
+  base: '/portfolios/',   // ✅ REQUIRED for your folder structure
   plugins: [react()],
   server: {
     port: 5173
